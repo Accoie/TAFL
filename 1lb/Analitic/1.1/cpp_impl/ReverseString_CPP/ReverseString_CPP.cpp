4c3320affe005692329fef373f091e73b2d89ae7
Никита Касаткин <nk1kasatkin@yandex.ru>
2025-09-24 13:55:57 +0300

    Добавьте файлы проекта.

---
+++ 1lb/Analitic/1.1/cpp_impl/ReverseString_CPP/ReverseString_CPP.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string ReverseString(const string& input)
+{
+    string result;
+
+    for (int i = input.length() - 1; i >= 0; i--)
+    {
+        result += input[i];
+    }
+
+    return result;
+}
+
+int main()
+{
+    cout << "your string:" << endl;
+    string input;
+    getline(cin, input);
+
+    if (input.empty())
+    {
+        cout << "empty input" << endl;
+        return 0;
+    }
+
+    string reversed = ReverseString(input);
+    cout << "reverse string: " << reversed << endl;
+
+    return 0;
+}
